241355895f4b42e875e5f8a530662a21d7792add
dingshiyang <dingshiyang@126.com>
2014-11-25 21:30:43 +0800

    第三次测试提交 2014年11月25日

--- src/com/dream/test/First.java
+++ src/com/dream/test/First.java
@@ -6,8 +6,15 @@ public class First {
 	{
 		System.out.println("It is my dream!");
 		System.out.println("My dream will do first!");
+		add();
 	}
 	
-	
+	private static  void add()
+	{
+		int i = 1;
+		int j = 3;
+		int k = i+j;
+		System.out.println("1+3的和为:"+k);
+	}
 	
 }
