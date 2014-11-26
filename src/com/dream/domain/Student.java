4c1ce0b38f82c6dbc9a4eb1a22a36b4ddcb7ef08
dingshiyang <dingshiyang@126.com>
2014-11-26 14:47:31 +0800

    单元测试

---
+++ src/com/dream/domain/Student.java
@@ -0,0 +1,71 @@
+package com.dream.domain;
+
+public class Student {
+
+	private String id;
+	
+	private String name;
+	
+	private String address;
+	
+	private String phone;
+	
+	
+	private String sex;
+	
+	private String age;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/com/dream/test/StudentTest.java
@@ -0,0 +1,20 @@
+package com.dream.test;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dream.domain.Student;
+
+public class StudentTest {
+
+	@Test
+	public void testStudent()
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Student student = context.getBean("student",Student.class);
+		student.setAddress("1111");
+		System.out.println("ddd"+student.getAddress());
+	}
+	
+}
